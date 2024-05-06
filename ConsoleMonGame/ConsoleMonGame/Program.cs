eb8c3b92c8e0406ccad9ce061e4820ae882b8597
37955 <146433509+37955@users.noreply.github.com>
2024-05-06 12:20:02 +0200

    ConsoleMonGame

--- ConsoleMonGame/ConsoleMonGame/Program.cs
+++ ConsoleMonGame/ConsoleMonGame/Program.cs
@@ -7,4 +7,11 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+    internal class ConsoleMon
+    {
+        internal int health;
+        internal int energy;
+        internal string name;
+    }
 }
