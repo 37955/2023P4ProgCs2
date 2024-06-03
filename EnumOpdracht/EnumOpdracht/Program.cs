f9da5912a583a3a95f42b63cf09c337694d2dde5
Pieter van der Schoot <37955@ma-web.nl>
2024-06-03 09:44:46 +0200

    EnumOpdracht

---
+++ EnumOpdracht/EnumOpdracht/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EnumOpdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Colors myMainThemeColor = Colors.Purple;
+
+            if (myMainThemeColor == Colors.Purple)
+            {
+                Console.WriteLine("Dat is mijn kleur!");
+            }
+
+            Colors[] allColors = (Colors[])Enum.GetValues(typeof(Colors));
+            foreach (Colors color in allColors)
+            {
+                Console.WriteLine(color);
+            }
+
+            Console.WriteLine("Voer een kleur in (Purple, Red, Blue, Green):");
+            string value = Console.ReadLine();
+            Colors parsedColor;
+            if (Enum.TryParse(value, true, out parsedColor))
+            {
+                Console.WriteLine($"Je hebt {parsedColor} gekozen.");
+            }
+            else
+            {
+                Console.WriteLine("Ongeldige kleur ingevoerd.");
+            }
+        }
+    }
+}
