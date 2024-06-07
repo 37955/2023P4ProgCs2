b5f74af8cc1cb5de94484e8688a4f5a3860d5a87
Pieter van der Schoot <37955@ma-web.nl>
2024-06-07 13:55:48 +0200

    FileDirOpdracht

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryInfoExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\pjotr\OneDrive - Mediacollege Amsterdam\ma\bewijzenmap\1.4\2023P4ProgCs2";
+ 
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            FileInfo[] files = dir.GetFiles();
+            
+            Console.WriteLine("Files:");
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
+            
+            DirectoryInfo[] directories = dir.GetDirectories();
+            
+            Console.WriteLine("\nDirectories:");
+            foreach (DirectoryInfo directory in directories)
+            {
+                Console.WriteLine(directory.Name);
+            }
+        }
+    }
+}
